c750f04b503a299b1754e57fa747f61ecdc882ca
moisesRubens <moisesrubens.pro@gmail.com>
2026-01-07 19:12:12 -0300

    refactor: improve player routes

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\BattleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PlayerController::class, 'index'])->name('index');
-Route::get('/create', [PlayerController::class, 'create'])->name('create');
-Route::post('/', [PlayerController::class, 'store'])->name('store');
-Route::get('/{player}/edit', [PlayerController::class, 'edit'])->name('edit');
-Route::put('/{player}', [PlayerController::class, 'update'])->name('update');
-Route::delete('/{player}', [PlayerController::class, 'destroy'])->name('destroy');
-Route::post('/{player}/add-point', [PlayerController::class, 'addPoint'])->name('addPoint');
+Route::prefix('players')->group(function () {
+    Route::get('/', [PlayerController::class, 'index'])->name('players.index');
+    Route::get('/create', [PlayerController::class, 'create'])->name('players.create');
+    Route::post('/', [PlayerController::class, 'store'])->name('players.store');
+    Route::get('/{player}/edit', [PlayerController::class, 'edit'])->name('players.edit');
+    Route::put('/{player}', [PlayerController::class, 'update'])->name('players.update');
+    Route::delete('/{player}', [PlayerController::class, 'destroy'])->name('players.destroy');
+    Route::post('/{player}/add-point', [PlayerController::class, 'addPoint'])->name('players.addPoint');
+});
 
 Route::resource('partidas', BattleController::class);
