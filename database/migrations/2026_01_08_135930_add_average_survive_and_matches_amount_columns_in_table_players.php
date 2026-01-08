97322e60cc7ef4fc483e89f7c74ead53326b3ae1
moisesRubens <moisesrubens.pro@gmail.com>
2026-01-08 11:06:47 -0300

    feat: add average_survive and matches_amount columns in players table

---
+++ database/migrations/2026_01_08_135930_add_average_survive_and_matches_amount_columns_in_table_players.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->float( 'average_survive')->default(0)->nullable();
+            $table->integer('matches_amount')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn( 'average_survive');
+            $table->dropColumn('matches_amount');
+        });
+    }
+};
