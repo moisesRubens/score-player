af587e0cbbf49c74b6342ca0221a821eee81d144
moisesRubens <moisesrubens.pro@gmail.com>
2026-01-08 08:35:20 -0300

    refactor: web root

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\BattleController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [PlayerController::class, 'index'])->name('players.index');
+
 Route::prefix('players')->group(function () {
-    Route::get('/', [PlayerController::class, 'index'])->name('players.index');
     Route::get('/create', [PlayerController::class, 'create'])->name('players.create');
     Route::post('/', [PlayerController::class, 'store'])->name('players.store');
     Route::get('/{player}/edit', [PlayerController::class, 'edit'])->name('players.edit');
